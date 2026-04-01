618d6e29db2058db932b18b66156f61b411dff17
Qawerz <alex.qawerz@yandex.ru>
2026-04-02 00:50:58 +0300

    added sqlite3; computed all tags (and by filter)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from typing import Optional
 import operator as op 
 
@@ -8,20 +9,32 @@ from fastapi.templating import Jinja2Templates
 from datetime import datetime
 import re 
 
-app  = FastAPI()
+import sqlite3
+db_con = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_con = sqlite3.connect("qavabooru.db")
+    cursor = db_con.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Posts (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    image TEXT NOT NULL,
+    tags TEXT NOT NULL,
+    tags_count INTEGER NOT NULL
+    )
+    """)
+    db_con.commit()
+    yield
+    db_con.close()
+
+app  = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"))
 templates = Jinja2Templates(directory="templates")
 
 posts_db = [
-    {
-        "id": 1,
-        "title": "Castoria",
-        "content": "Это содержание первого поста.",
-        "author": "KateFox",
-        "created_at": "2023-10-01 10:00:00",
-        "image": "http://127.0.0.1:8000/static/img/1.png",
-        "tags":"girl purple hsr honka_star_rail flowers moon butterfly sycle kate_fox",
-    },
     {
         "id": 2,
         "title": "Blue eyes girl",
@@ -56,19 +69,34 @@ posts_db = [
 @app.get("/")
 @app.get("/posts")
 async def get_posts(request: Request, tags: Optional[str] = None, page: Optional[int] = 1):
-    posts_on_page = 3
+    con = sqlite3.connect("qavabooru.db")
+    cursor = con.cursor()
+    posts_on_page = 20
     pagintaion=[posts_on_page*page-posts_on_page, posts_on_page*page]
+
+    posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
+    posts_data = []
+    for post in posts_data_raw:
+        posts_data.append({"id":post[0], "title":post[1], "image":post[2], "tags":post[3]})
+    con.close()
     
+    grouped_tags = []
+
+    for post in posts_data[pagintaion[0]:pagintaion[1]]:
+        for tag in post["tags"].split(" "):
+            grouped_tags.append(tag)
+
+    grouped_tags = list(set(grouped_tags))
+    print(f"{posts_data=}")
     if tags:
         print(f"{tags=}")
         
         pos_tags = list(set([tag for tag in tags.strip().split(" ") if not tag.startswith("-")]))
         neg_tags = [tag[1:] for tag in tags.split(" ") if tag.startswith("-")]
-        for t in pos_tags:
-            print(type(t))
+
         print(f"{pos_tags=}\n{neg_tags=}")
         filtered = []
-        for item in posts_db[pagintaion[0]:pagintaion[1]]: # Проходимся по всему массиву
+        for item in posts_data[pagintaion[0]:pagintaion[1]]: # Проходимся по всему массиву
             filter_flag = True # Задаем, что пост проходит по фильтру
             for tag in pos_tags: # Проходимся по каждому тегу запроса
                 if tag not in item["tags"].split(" "): #
@@ -82,19 +110,38 @@ async def get_posts(request: Request, tags: Optional[str] = None, page: Optional
                 if tag in item["tags"]:
                     filtered.remove(item)
 
-        return templates.TemplateResponse(request, name="posts.html", context={"posts": filtered})
+        grouped_tags = []
 
-    return templates.TemplateResponse(request, name="posts.html", context={"posts": posts_db[pagintaion[0]:pagintaion[1]]})
+        for post in filtered:
+            for tag in post["tags"].split(" "):
+                grouped_tags.append(tag)
+        grouped_tags = list(set(grouped_tags))
+
+        return templates.TemplateResponse(request, name="posts.html", context={"posts": filtered, "grouped_tags": grouped_tags})
+
+    return templates.TemplateResponse(request, name="posts.html", context={"posts": posts_data[pagintaion[0]:pagintaion[1]], "grouped_tags": grouped_tags})
 
 @app.get("/posts.json")
-async def get_posts(request: Request, tags: Optional[str] = None):
+async def get_posts(request: Request, tags: Optional[str] = None, page: Optional[int]=1):
+    con = sqlite3.connect("qavabooru.db")
+    cursor = con.cursor()
+    posts_on_page = 3
+    pagintaion=[posts_on_page*page-posts_on_page, posts_on_page*page]
+
+    posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
+    posts_data = []
+    for post in posts_data_raw:
+        posts_data.append({"id":post[0], "title":post[1], "image":post[2], "tags":post[3]})
+    con.close()
+    
+    print(f"{posts_data=}")
     if tags:
         print(f"{tags=}")
         pos_tags = [tag for tag in tags.split(" ") if (not tag.startswith("-")) ]
         neg_tags = [tag[1:] for tag in tags.split(" ") if tag.startswith("-")]
         print(f"{pos_tags=}\n{neg_tags=}")
         filtered = []
-        for item in posts_db: # Проходимся по всему массиву
+        for item in posts_data[pagintaion[0]:pagintaion[1]]: # Проходимся по всему массиву
             filter_flag = True # Задаем, что пост проходит по фильтру
             for tag in pos_tags: # Проходимся по каждому тегу запроса
                 if tag not in item["tags"].split(" "): #
@@ -110,7 +157,7 @@ async def get_posts(request: Request, tags: Optional[str] = None):
 
         return {"posts": filtered}
 
-    return {"posts": posts_db}
+    return {"posts": posts_data[pagintaion[0]:pagintaion[1]]}
 
 
 @app.get("/posts/{post_msg}")
@@ -121,10 +168,20 @@ async def get_posts_by_id(request: Request, post_msg: str):
         raise HTTPException(status_code=400, detail="Invalid format")
     post_id = int(match.group(1))
     post_format = match.group(2)
+
+    con = sqlite3.connect("qavabooru.db")
+    cursor = con.cursor()
+
+    posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
+    posts_data = []
+    for post in posts_data_raw:
+        posts_data.append({"id":post[0], "title":post[1], "image":post[2], "tags":post[3]})
+    con.close()
     if not post_format:
-        return templates.TemplateResponse(request, name="post.html", context={"post":posts_db[post_id-1]})
+        return templates.TemplateResponse(request, name="post.html", context={"post":posts_data[post_id-1]})
     elif post_format == ".json":
         return {
-            "post":posts_db[post_id-1]
+            "post":posts_data[post_id-1]
         }
+
 
