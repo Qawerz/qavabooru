eb807b75619864f9c761d7b2e7391e3c5eddf7f8
Qawerz <alex.qawerz@yandex.ru>
2026-04-02 17:23:57 +0300

    add some stylish

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ db_con = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    db_con = sqlite3.connect("qavabooru.db")
+    db_con = sqlite3.connect("qbooru.db")
     cursor = db_con.cursor()
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Posts (
@@ -34,44 +34,12 @@ app  = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"))
 templates = Jinja2Templates(directory="templates")
 
-posts_db = [
-    {
-        "id": 2,
-        "title": "Blue eyes girl",
-        "content": "Это содержание второго поста.",
-        "author": "Пётр Петров",
-        "created_at": "2023-10-02 15:30:00",
-        "image": "http://127.0.0.1:8000/static/img/2.png",
-        "tags":"girl white_shirt blue_eyes long_hair",
-    },
-    {
-        "id": 3,
-        "title": "Bloody Forest",
-        "content": "Это содержание второго поста.",
-        "author": "Qawerz",
-        "created_at": "2023-10-02 15:30:00",
-        "image": "http://127.0.0.1:8000/static/img/3.png",
-        "tags":"girl blask_shirt skirt short_hair red forest foggy school_uniform",
-    },
-    {
-        "id": 4,
-        "title": "1 girl,frog hat,gray hair,atmosphere background,cyan eyes,white shirt,portrait,aesthetic,hipster clothes,",
-        "image": "http://127.0.0.1:8000/static/img/4.jpg",
-        "tags":"1_girl frog_hat gray_hair atmosphere_background cyan_eyes white_shirt portrait aesthetic hipster_clothes"
-    },
-    {
-        "id": 5,
-        "title": "02111-428668394-1 girl,dress with a flower pattern,rim light,chromatic aberation,(rough sketch_1.2),gradient hair,blue hair tone,pink hair tone,.jpg",
-        "image": "http://127.0.0.1:8000/static/img/5.jpg",
-        "tags":"1_girl dress_with_a_flower_pattern rim_light chromatic_aberation (rough_sketch_1.2) gradient_hair blue_hair_tone pink_hair_tone"
-    }
-]
 @app.get("/")
 @app.get("/posts")
 async def get_posts(request: Request, tags: Optional[str] = None, page: Optional[int] = 1):
-    con = sqlite3.connect("qavabooru.db")
+    con = sqlite3.connect("qbooru.db")
     cursor = con.cursor()
-    posts_on_page = 20
+    posts_on_page = 40
     pagintaion=[posts_on_page*page-posts_on_page, posts_on_page*page]
 
     posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
@@ -79,7 +47,7 @@ async def get_posts(request: Request, tags: Optional[str] = None, page: Optional
     for post in posts_data_raw:
         posts_data.append({"id":post[0], "title":post[1], "image":post[2], "tags":post[3]})
     con.close()
-    
+    posts_data.reverse()
     grouped_tags = []
 
     for post in posts_data[pagintaion[0]:pagintaion[1]]:
@@ -87,16 +55,16 @@ async def get_posts(request: Request, tags: Optional[str] = None, page: Optional
             grouped_tags.append(tag)
 
     grouped_tags = list(set(grouped_tags))
-    print(f"{posts_data=}")
+    # print(f"{posts_data=}")c
     if tags:
-        print(f"{tags=}")
+        # print(f"{tags=}")c
         
         pos_tags = list(set([tag for tag in tags.strip().split(" ") if not tag.startswith("-")]))
-        neg_tags = [tag[1:] for tag in tags.split(" ") if tag.startswith("-")]
+        neg_tags = list(set([tag[1:] for tag in tags.split(" ") if tag.startswith("-")]))
 
-        print(f"{pos_tags=}\n{neg_tags=}")
+        # print(f"{pos_tags=}\n{neg_tags=}")
         filtered = []
-        for item in posts_data[pagintaion[0]:pagintaion[1]]: # Проходимся по всему массиву
+        for item in posts_data: # Проходимся по всему массиву
             filter_flag = True # Задаем, что пост проходит по фильтру
             for tag in pos_tags: # Проходимся по каждому тегу запроса
                 if tag not in item["tags"].split(" "): #
@@ -117,13 +85,27 @@ async def get_posts(request: Request, tags: Optional[str] = None, page: Optional
                 grouped_tags.append(tag)
         grouped_tags = list(set(grouped_tags))
 
-        return templates.TemplateResponse(request, name="posts.html", context={"posts": filtered, "grouped_tags": grouped_tags})
+        paggination ={
+            "current": page,
+            "next": page+1 if len(filtered)+posts_on_page > 0 else None,
+            "prev": page-1 if len(filtered)-posts_on_page < 0 else None
+        }
 
-    return templates.TemplateResponse(request, name="posts.html", context={"posts": posts_data[pagintaion[0]:pagintaion[1]], "grouped_tags": grouped_tags})
+
+
+        return templates.TemplateResponse(request, name="posts.html", context={"posts": filtered[pagintaion[0]:pagintaion[1]], "grouped_tags": grouped_tags, "paggination": paggination})
+
+    paggination ={
+        "current": page,
+        "next": page+1 if posts_on_page*page+1 < len(posts_data) else None,
+        "prev": page-1 if len(posts_data)-posts_on_page > 0 else None
+    }
+    print(f"{paggination}\n{len(posts_data)=}\n{posts_on_page=}")
+    return templates.TemplateResponse(request, name="posts.html", context={"posts": posts_data[pagintaion[0]:pagintaion[1]], "grouped_tags": grouped_tags, "paggination": paggination})
 
 @app.get("/posts.json")
 async def get_posts(request: Request, tags: Optional[str] = None, page: Optional[int]=1):
-    con = sqlite3.connect("qavabooru.db")
+    con = sqlite3.connect("qbooru.db")
     cursor = con.cursor()
     posts_on_page = 3
     pagintaion=[posts_on_page*page-posts_on_page, posts_on_page*page]
@@ -169,7 +151,7 @@ async def get_posts_by_id(request: Request, post_msg: str):
     post_id = int(match.group(1))
     post_format = match.group(2)
 
-    con = sqlite3.connect("qavabooru.db")
+    con = sqlite3.connect("qbooru.db")
     cursor = con.cursor()
 
     posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
@@ -181,7 +163,27 @@ async def get_posts_by_id(request: Request, post_msg: str):
         return templates.TemplateResponse(request, name="post.html", context={"post":posts_data[post_id-1]})
     elif post_format == ".json":
         return {
-            "post":posts_data[post_id-1]
+            "post":posts_data[post_id]
         }
 
 
+@app.get("/all_tags")
+async def get_all_tags(request: Request):
+    con = sqlite3.connect("qbooru.db")
+    cursor = con.cursor()
+
+
+    posts_data_raw = cursor.execute("SELECT * FROM Posts").fetchall()
+    posts_data = []
+    for post in posts_data_raw:
+        posts_data.append({"id":post[0], "title":post[1], "image":post[2], "tags":post[3]})
+    con.close()
+    
+    grouped_tags = []
+
+    for post in posts_data:
+        for tag in post["tags"].split(" "):
+            grouped_tags.append(tag)
+
+    grouped_tags = list(set(grouped_tags))
+    return templates.TemplateResponse(request, name="all_tags.html", context={"tags":grouped_tags})
